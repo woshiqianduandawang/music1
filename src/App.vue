0d8cd120a8e4d623f92a37469e417bc68a251ba4
cc <1174149547@qq.com>
2022-12-30 00:20:47 +0800

    更新了自动识别并跳转到移动端页面

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,38 @@
   <div id="app">
     <Hade></Hade>
     <keep-alive>
-      <router-view/>
+      <router-view />
     </keep-alive>
     <Player v-if="$store.state.playerif"></Player>
   </div>
 </template>
 
 <script>
-import Hade from './components/content/head.vue'
-import Player from '@/components/content/player';
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
-// 执行相应代码或直接跳转到手机页面
-window.location.href="https://miniccmusic.netlify.app/";
-} else {
-
-// 执行桌面端代码
-}
+import Hade from "./components/content/head.vue";
+import Player from "@/components/content/player";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Hade,
-    Player
+    Player,
   },
-}
+  beforeCreate() {
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)
+    ) {
+      // 执行相应代码或直接跳转到手机页面
+      window.location.href = "https://miniccmusic.netlify.app/";
+    } else {
+      // 执行桌面端代码
+    }
+  },
+};
 </script>
 
 <style>
 @import url(./assets/css/base.css);
-  #app{
-    box-shadow: 1px 1px 5px rgb(0, 0, 0, 0.3);
-  }
+#app {
+  box-shadow: 1px 1px 5px rgb(0, 0, 0, 0.3);
+}
 </style>
